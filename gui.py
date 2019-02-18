7d333c781d784a2287552da690b8c628644298e2
Mario <elasgard@hotmail.es>
2019-02-18 11:38:25 +0100

    first-commit2

---
+++ gui.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'qt_gui.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.setEnabled(True)
+        MainWindow.resize(419, 454)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("../leap/PCRecognizer_LEAP/background.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        MainWindow.setTabShape(QtGui.QTabWidget.Rounded)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.rbutton_video = QtGui.QRadioButton(self.centralwidget)
+        self.rbutton_video.setGeometry(QtCore.QRect(30, 80, 82, 17))
+        self.rbutton_video.setChecked(True)
+        self.rbutton_video.setObjectName(_fromUtf8("rbutton_video"))
+        self.rbutton_playlist = QtGui.QRadioButton(self.centralwidget)
+        self.rbutton_playlist.setGeometry(QtCore.QRect(130, 80, 82, 17))
+        self.rbutton_playlist.setObjectName(_fromUtf8("rbutton_playlist"))
+        self.textEdit_link = QtGui.QTextEdit(self.centralwidget)
+        self.textEdit_link.setGeometry(QtCore.QRect(30, 50, 351, 21))
+        self.textEdit_link.setAcceptDrops(True)
+        self.textEdit_link.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.textEdit_link.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.textEdit_link.setObjectName(_fromUtf8("textEdit_link"))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(30, 22, 101, 16))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.label.setFont(font)
+        self.label.setLineWidth(1)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.button_download = QtGui.QPushButton(self.centralwidget)
+        self.button_download.setGeometry(QtCore.QRect(30, 210, 111, 23))
+        self.button_download.setObjectName(_fromUtf8("button_download"))
+        self.line = QtGui.QFrame(self.centralwidget)
+        self.line.setGeometry(QtCore.QRect(30, 190, 351, 16))
+        self.line.setFrameShape(QtGui.QFrame.HLine)
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setObjectName(_fromUtf8("line"))
+        self.progressbar = QtGui.QProgressBar(self.centralwidget)
+        self.progressbar.setGeometry(QtCore.QRect(260, 210, 121, 23))
+        font = QtGui.QFont()
+        font.setKerning(True)
+        self.progressbar.setFont(font)
+        self.progressbar.setProperty("value", 0)
+        self.progressbar.setTextVisible(False)
+        self.progressbar.setObjectName(_fromUtf8("progressbar"))
+        self.button_search = QtGui.QPushButton(self.centralwidget)
+        self.button_search.setGeometry(QtCore.QRect(310, 160, 75, 23))
+        self.button_search.setObjectName(_fromUtf8("button_search"))
+        self.textEdit_folder = QtGui.QTextEdit(self.centralwidget)
+        self.textEdit_folder.setEnabled(True)
+        self.textEdit_folder.setGeometry(QtCore.QRect(30, 160, 261, 21))
+        self.textEdit_folder.setAcceptDrops(True)
+        self.textEdit_folder.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.textEdit_folder.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.textEdit_folder.setObjectName(_fromUtf8("textEdit_folder"))
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(30, 130, 111, 16))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.textEdit_logs = QtGui.QTextEdit(self.centralwidget)
+        self.textEdit_logs.setEnabled(True)
+        self.textEdit_logs.setGeometry(QtCore.QRect(30, 250, 351, 161))
+        self.textEdit_logs.setAcceptDrops(True)
+        self.textEdit_logs.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.textEdit_logs.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.textEdit_logs.setReadOnly(True)
+        self.textEdit_logs.setObjectName(_fromUtf8("textEdit_logs"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 419, 21))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "YouTube Downloader", None))
+        self.rbutton_video.setText(_translate("MainWindow", "Single Video", None))
+        self.rbutton_playlist.setText(_translate("MainWindow", "Playlist", None))
+        self.label.setText(_translate("MainWindow", "Youtube link", None))
+        self.button_download.setText(_translate("MainWindow", "Start Download", None))
+        self.button_search.setText(_translate("MainWindow", "Search", None))
+        self.label_2.setText(_translate("MainWindow", "Output Folder", None))
+
---
+++ yt_dwn.py
@@ -0,0 +1,173 @@
+from pytube import YouTube
+from bs4 import BeautifulSoup
+import sys
+import time
+import os
+import requests
+import threading
+
+from gui import *
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+
+"""def dl_video(link, ofolder):
+	print("downloading video with link: \n"+str(link))
+	#mw.textEdit_logs.append("downloading video with link: \n"+str(video.get("href")))
+	YouTube(link).streams.first().download(ofolder)
+	print("done!")
+	#mw.textEdit_logs.append("done")
+				
+def dl_playlist(link, ofolder):
+	html_page = requests.get(link)
+	soup = BeautifulSoup(html_page.text, "html.parser")
+	res = soup.find_all("a", {"class":"pl-video-title-link"})
+
+	print(str(len(res))+" videos in playlist "+str(link))
+	mw.textEdit_logs.append(str(len(res))+" videos in playlist "+str(link))
+	for video in res:
+		print("downloading video with link: \n"+str(video.get("href")))
+		#mw.textEdit_logs.append("downloading video with link: \n"+str(video.get("href")))
+		YouTube("https://www.youtube.com/"+video.get("href")).streams.first().download(ofolder)
+		print("done!")
+		#mw.textEdit_logs.append("done")"""
+
+
+class mwThread(QThread):
+    def __init__(self, links, folder):
+		QThread.__init__(self)
+		self.links = links
+		self.ofolder = folder
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+		for link in self.links:
+			print("Downloading video with link: \n"+str(link))
+			self.emit(SIGNAL("print_text(QString)"), "Downloading video with link: \n"+str(link))
+			YouTube(link).streams.first().download(self.ofolder)
+			print("Done!\n")
+			self.emit(SIGNAL("print_text(QString)"), "done!\n")
+		
+class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
+	playlist = False
+	downloading = False
+	
+	def __init__(self, *args, **kwargs):
+		QtGui.QMainWindow.__init__(self, *args, **kwargs)
+		self.setupUi(self)
+
+		self.button_search.clicked.connect(self.search_folder)
+		self.button_download.clicked.connect(self.download)
+		self.rbutton_video.toggled.connect(lambda: self.rbutton_changed("video"))
+		self.rbutton_playlist.toggled.connect(lambda: self.rbutton_changed("playlist"))
+
+	def search_folder(self):
+		folder = QFileDialog.getExistingDirectory(self, "Output Folder")
+		print(folder)
+		self.textEdit_folder.setText(folder)
+
+	def rbutton_changed(self, typ):
+		print(typ)
+		if typ == "playlist":
+			self.playlist = True
+		else:
+			self.playlist = False
+		
+	def download(self):
+		if self.textEdit_folder.toPlainText() != "" and self.textEdit_link.toPlainText() != "":
+			if self.playlist:					
+				#dl_playlist(str(self.textEdit_link.toPlainText()), str(self.textEdit_folder.toPlainText()))
+				links = self.get_playlist_links(str(self.textEdit_link.toPlainText()))
+				self.progressbar.setMaximum(2*len(links))
+				
+			else:
+				links = []
+				links.append(str(self.textEdit_link.toPlainText()))
+				self.progressbar.setMaximum(2)
+				#dl_video(str(self.textEdit_link.toPlainText()), str(self.textEdit_folder.toPlainText()))
+				time.sleep(1)
+				#self.progressbar.setValue(100)
+				
+			self.mw_thread = mwThread(links, str(self.textEdit_folder.toPlainText()))
+			self.connect(self.mw_thread, SIGNAL("print_text(QString)"), self.print_text)
+			self.connect(self.mw_thread, SIGNAL("finished()"), self.download_finished)
+			self.mw_thread.start()
+
+	def get_playlist_links(self, plink):
+		html_page = requests.get(plink)
+		soup = BeautifulSoup(html_page.text, "html.parser")
+		res = soup.find_all("a", {"class":"pl-video-title-link"})
+		for c in range(len(res)):
+			res[c] = "https://www.youtube.com/"+res[c].get("href")
+			
+		return res
+
+	def print_text(self, text):
+		self.progressbar.setValue(self.progressbar.value()+1)
+		self.textEdit_logs.append(text)
+
+	def download_finished(self):
+		if self.playlist:
+			self.textEdit_logs.append("\nplaylist download finished!\n")
+		QtGui.QMessageBox.information(self, "Done!", "Download finished")
+		self.progressbar.setValue(0)
+				
+	def keyPressEvent(self, event):
+		if event.key() == QtCore.Qt.Key_Q:
+			self.close()
+			sys.exit()
+
+		
+if __name__ == "__main__":
+	"""
+	url = "https://www.youtube.com/watch?v=FckQc1Fr5jI"
+	playlist = "https://www.youtube.com/playlist?list=PL5IVLTK9PtksBYBQZOPd_39avcDTknAMU"
+	path_to_save = "E:\ESTUDIO\PROGRAMACION\Python\yt_dwn"
+	"""
+
+	"""app = QtGui.QApplication(sys.argv)
+	mw = MainWindow()
+	mw.show()
+	app.exec_()"""
+	
+	if len(sys.argv) == 2 and sys.argv[1] == "-gui":
+		app = QtGui.QApplication(["YT downloader"])
+		mw = MainWindow()
+		mw.show()
+		app.exec_()
+		
+	elif len(sys.argv) != 5 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+		print("way to use: ")
+		print("CONSOLE mode: python yt_dnw.py [-v|--video | -p|--playlist] url -l|--location path_where_to_save")
+		print("GUI mode: python yt_dnw.py -gui")
+		
+	else:
+		if "-l" in sys.argv or "--location" in sys.argv:
+			path_to_save = str(sys.argv[4])
+			if not os.path.exists(path_to_save) or not os.path.isdir(path_to_save):
+				print("given directory not valid")
+				sys.exit()
+				
+			if sys.argv[1] == "-p" or sys.argv[1] == "--playlist":
+				# we have a playlist link
+				playlist = str(sys.argv[2])
+				html_page = requests.get(playlist)
+				soup = BeautifulSoup(html_page.text, "html.parser")
+				res = soup.find_all("a", {"class":"pl-video-title-link"})
+
+				print(str(len(res))+" videos in playlist "+str(playlist))
+				for link in res:
+					print("downloading video with link: \n"+str(link.get("href")))
+					YouTube("https://www.youtube.com/"+link.get("href")).streams.first().download(path_to_save)
+
+			elif sys.argv[1] == "-v" or sys.argv[1] == "--video":
+				# we have a single video link
+				video_url = str(sys.argv[2])
+				print("downloading video with link: \n"+str(video_url))
+				YouTube(video_url).streams.first().download(path_to_save)
+
+		else:
+			print("you need to specify a path to save your download/s (-l|--location path_where_to_save)")
+			sys.exit()
